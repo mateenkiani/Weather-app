8d8974c490689c76a63bfdd2538dee1e724ad407
Mateen Kiani <kiani.mateen012@gmail.com>
2020-01-17 12:04:29 +0500

    Renamed files

--- lib/main.dart
+++ lib/main.dart
@@ -3,50 +3,19 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Weather App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Home'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            RaisedButton(
-              color: Colors.blueAccent,
-              onPressed: () => setState(() {
-                print("you have clicked the button");
-              }),
-              child: Text('Click Me'),
-            ),
-          ],
+    return Container(
+      padding: EdgeInsets.all(20),
+      decoration: BoxDecoration(color: Colors.orange),
+      child: Text(
+        'Hello World',
+        textDirection: TextDirection.ltr,
+        style: TextStyle(
+          fontSize: 32,
+          color: Colors.black87,
         ),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 }
